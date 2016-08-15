00a5165349d204fda956c8c50d9ff1cac3aa4eb5
dotnet.sahookashi@gmail.com <dotnet.sahookashi@gmail.com>
2016-08-15 16:32:47 +0530

    Revert "hi its second commit"
    This reverts commit 54009e6a3d9d43358f53f55dcd89ccbecc52f96d.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,7 +8,6 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        //this is my first project to set all the thing
         static void Main(string[] args)
         {
             Console.Write("In the new solution");
